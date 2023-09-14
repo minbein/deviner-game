772001059f7ba3ccb7c80e635acd0d2f5f69eb64
Luiz Gasparetto <luizegasp@gmail.com>
2023-09-14 09:36:42 -0300

    fix: image path and styles path

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -1,42 +1,48 @@
 document.addEventListener("DOMContentLoaded", function () {
-    function toggleDarkMode() {
-      const body = document.body;
-      const darkModeButton = document.getElementById("darkModeButton");
-      const themeIcon = document.getElementById("themeIcon");
-      const head = document.head;
-      const darkModeStylesheet = document.createElement("link");
-      
-      
-      darkModeStylesheet.href = "./styles/darkmode.css";
-      darkModeStylesheet.rel = "stylesheet";
-  
-      darkModeButton.addEventListener("click", () => {
-        console.log("Botão de alternância de tema clicado.");
-        body.classList.toggle("dark");
-  
-        if (body.classList.contains("dark")) {
-          themeIcon.src = "/src/moon.png";
-          themeIcon.alt = "Modo Escuro";
-  
-          const existingStylesheet = document.querySelector(
-            'link[href="./styles/darkmode.css"]'
-          );
-          if (!existingStylesheet) {
-            head.appendChild(darkModeStylesheet);
-          }
-        } else {
-          themeIcon.src = "/src/sun.png";
-          themeIcon.alt = "Modo Claro";
-  
-          const existingStylesheet = document.querySelector(
-            'link[href="./styles/darkmode.css"]'
-          );
-          if (existingStylesheet) {
-            head.removeChild(existingStylesheet);
-          }
+  function toggleDarkMode() {
+    const body = document.body;
+    const darkModeButton = document.getElementById("darkModeButton");
+    const themeIcon = document.getElementById("themeIcon");
+    const head = document.head;
+    const darkModeStylesheet = document.createElement("link");
+
+
+    darkModeStylesheet.href = "../styles/darkmode.css";
+    darkModeStylesheet.rel = "stylesheet";
+
+    darkModeButton.addEventListener("click", () => {
+      console.log("Botão de alternância de tema clicado.");
+      body.classList.toggle("dark");
+
+      if (body.classList.contains("dark")) {
+        themeIcon.src = "./src/moon.png";
+        themeIcon.alt = "Modo Escuro";
+
+        const existingStylesheet = document.querySelector(
+          'link[href="../styles/darkmode.css"]'
+        );
+        if (!existingStylesheet) {
+          head.appendChild(darkModeStylesheet);
         }
-      });
-    }
-  
-    toggleDarkMode();
-  });
+      } else {
+        themeIcon.src = "./src/sun.png";
+        themeIcon.alt = "Modo Claro";
+
+        const existingStylesheet = document.querySelector(
+          'link[href]="./styles/darkmode.css"]'
+        );
+        if (existingStylesheet) {
+          head.removeChild(existingStylesheet);
+        }
+      }
+
+      if (body.classList.contains("dark")) {
+        console.log("A classe 'dark' foi adicionada ao body.");
+      } else {
+        console.log("A classe 'dark' foi removida do body.");
+      }
+    });
+  }
+
+  toggleDarkMode();
+});
